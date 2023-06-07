ae261bea3be9e499623a8f7190bb65341ceba5e6
socean <lumengeared@gmail.com>
2023-06-07 17:22:32 +0800

    progress

---
+++ src/auth/mod.rs
@@ -0,0 +1,67 @@
+use std::borrow::Cow;
+
+use anyhow::{anyhow, Result};
+use poise::serenity_prelude::{self as serenity, Mention};
+
+use crate::{operations, Data};
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+use super::util;
+
+#[derive(Clone, Debug, Copy)]
+pub enum Auth {
+    User,
+    Admin,
+    Role(serenity::RoleId),
+}
+
+pub async fn is_user_authorised_for_action(
+    ctx: &Context<'_>,
+    member: Cow<'_, serenity::Member>,
+    required: Auth,
+) -> Result<bool> {
+    match required {
+        Auth::User => Ok(true),
+        Auth::Admin => Ok(member.permissions(&ctx).unwrap().manage_guild()),
+        Auth::Role(needed_role_id) => {
+            match member.user.has_role(ctx, ctx.guild_id().unwrap(), needed_role_id).await {
+                Ok(has_role) => Ok(has_role),
+                Err(e) => Err(anyhow!(e.to_string())),
+            }
+        }
+    }
+}
+
+pub async fn respond_based_on_auth_context(ctx: &Context<'_>, required: Auth) -> Result<bool> {
+    match is_user_authorised_for_action(&ctx, ctx.author_member().await.unwrap(), required).await {
+        Ok(allowed) => match allowed {
+            true => Ok(true),
+            false => {
+                let formatted = match required {
+                    Auth::User => "`user`".to_owned(),
+                    Auth::Admin => "`admin`".to_owned(),
+                    // Auth::Role(id) => format!("<@&{}>", id.0),
+                    Auth::Role(id) => util::resolve_role(&ctx, id)
+                        .await
+                        .map(|role| role.name)
+                        .unwrap_or("unknown".to_owned()),
+                };
+                match ctx
+                    .send(|builder| {
+                        builder.reply(true).content(format!(
+                            "You are not authorised to use this command. Requires {}",
+                            formatted
+                        ))
+                    })
+                    .await
+                {
+                    Ok(_) => Ok(false),
+                    Err(e) => Err(anyhow!(e.to_string())),
+                }
+            }
+        },
+        Err(e) => Err(anyhow!(e.to_string())),
+    }
+}
--- src/commands/confessions/mod.rs
+++ src/commands/confessions/mod.rs
@@ -1,10 +1,38 @@
-// use poise::serenity_prelude as serenity;
-// use tracing::info;
+use poise::serenity_prelude as serenity;
+use tracing::info;
 
-// // this is a blank struct initialised in main.rs and then imported here
-// use crate::{operations, Data};
+// this is a blank struct initialised in main.rs and then imported here
+use crate::{operations, Data};
 
-// type Error = Box<dyn std::error::Error + Send + Sync>;
-// type Context<'a> = poise::Context<'a, Data, Error>;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
 
-// use super::super::operations::channels::ChannelUse;
+use super::super::operations::channels::ChannelUse;
+
+#[poise::command(
+    slash_command,
+    prefix_command,
+    description_localized("en-GB", "Confesses"),
+    guild_only = true
+)]
+pub async fn confess(
+    ctx: Context<'_>,
+    #[description = "Context"] channel_use: ChannelUse,
+) -> Result<(), Error> {
+    let confession_channels_result = operations::channels::get_channels_in_guild_with_use(
+        &ctx.data().database,
+        ctx.guild_id().unwrap().0,
+        channel_use,
+    )
+    .await;
+    let parsed_confession_channels_result =
+        confession_channels_result.map(|c| c.into_iter().map(|c| c.id).collect::<Vec<u64>>());
+    let response = match parsed_confession_channels_result {
+        Ok(r) => serde_json::to_string(&r).unwrap_or("Error serialising channels".to_owned()),
+        Err(e) => e.to_string(),
+    };
+    if let Err(why) = ctx.say(response).await {
+        info!("Error sending message: {:?}", why);
+    }
+    Ok(())
+}
