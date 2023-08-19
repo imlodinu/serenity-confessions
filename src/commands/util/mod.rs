521580b120ce9d1db60034c59a01e09e48c59276
imlodinu <lodinu.kalugalage@student.curtin.edu.au>
2023-08-20 00:57:14 +0800

    fix

--- src/commands/util/mod.rs
+++ src/commands/util/mod.rs
@@ -11,29 +11,21 @@ type Context<'a> = poise::Context<'a, Data, Error>;
 #[poise::command(slash_command, prefix_command, guild_only = true)]
 pub async fn ping_vc(
     ctx: Context<'_>,
-    channel: Option<serenity::ChannelId>,
+    channel: serenity::ChannelId,
 ) -> Result<(), Error> {
-    for (channel_id, channel_value) in ctx.guild().unwrap().channels {
-        if let serenity::Channel::Guild(channel_value) = channel_value {
-        let members = channel_value.members(ctx).await?;
-        let is_looking_channel = if channel.is_some() {
-            channel_id == channel.unwrap().0
-        } else { members.iter().any(|x| x.user.id == ctx.author().id ) };
-        if !is_looking_channel {
-            continue;
-        }
-            if channel_value.kind == serenity::ChannelType::Voice {
-                let mut ping_string = String::from("Pinging:\n");
-                for member in members {
-                    ping_string.push_str(&format!("<@{}> ", member.user.id));
-                }
-                ctx.say(ping_string).await?;
-                return Ok(());
-            } else {
-                ctx.say("Channel not found").await?;
+    if let Ok(serenity::Channel::Guild(manifested_channel)) =  channel.to_channel(ctx).await {
+        if manifested_channel.kind == serenity::ChannelType::Voice {
+            let members = manifested_channel.members(ctx).await?;
+            let mut ping_string = String::from("Pinging: ");
+            for member in members {
+                ping_string.push_str(&format!("<@{}> ", member.user.id));
             }
+            ctx.say(ping_string).await?;
+        } else {
+            ctx.say("Channel not found").await?;
         }
+    } else {
+        ctx.say("Channel not found").await?;
     }
-    ctx.say("Channel not found").await?;
     Ok(())
 }
